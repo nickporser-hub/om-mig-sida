95f87e4c5e58caf18ebe240504d740e08b2fff72
Nick Porser <nick.porser@skola.taby.se>
2025-10-15 21:52:49 +0200

    LatestVersion

--- logic.js
+++ logic.js
@@ -1,9 +1,26 @@
+/*
 setInterval(SpawnImage, 20000);
 setTimeout(SpawnImage, 3000)
 
+
+const images = [];
+
 function SpawnImage()
 {
+    const ad = createElement("div");
+    ad.classList.add("ad");
 
+    document.body.appendChild(ad);
+
+    images.push(ad);
+
+    ad.addEventListener("mouseDown", (e) =>
+    {
+        if (e.button !== 0) return;
+
+    }) 
 }
+*/
+
 
 
